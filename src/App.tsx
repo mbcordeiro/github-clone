4874758a04b28c4bf98f5cee207eae4bc78da794
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-09-17 12:54:48 -0300

    Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import GlobalStyles from './styles/GlobalStyles';
+import Header from "./components/Header";
+import Profile from "./pages/Profile";
+import Repo from "./pages/Repo";
+//import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username/:repo" element={<Repo />} />
+        <Route path="/:username" element={<Profile />} />
+      </Routes>
+      
+      {/*<Footer />*/}
+
+      <GlobalStyles />
+    </BrowserRouter>
   );
 }
 
