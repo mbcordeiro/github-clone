fcba975abb7b38ea9ea6d304751eb1baffdfdaac
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-10-19 12:44:55 -0300

    Page repo

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
-  return <div />;
-}
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={"username"} to={"/mbcordeiro"}>
+          Matheus Cordeiro
+        </Link>
+
+        <span></span>
+
+        <Link className={"reponame"} to={"/mbcordeiro/ecoleta"}>
+          ecoleta
+        </Link>
+      </Breadcrumb>
+
+      <p>Description</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/mbcordeiro/ecoleta"}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
+};
 
 export default Repo;
