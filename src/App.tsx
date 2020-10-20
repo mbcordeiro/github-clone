4290a2b88e6a5779a1ed4d06b3848be7f0547cda
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-10-20 12:46:44 -0300

    Nav header and footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import 'react-calendar-heatmap/dist/styles.css';
+import "react-calendar-heatmap/dist/styles.css";
 
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import GlobalStyles from './styles/GlobalStyles';
+import GlobalStyles from "./styles/GlobalStyles";
 import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import Repo from "./pages/Repo";
-//import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -19,8 +19,8 @@ function App() {
         <Route path="/:username/:repo" element={<Repo />} />
         <Route path="/:username" element={<Profile />} />
       </Routes>
-      
-      {/*<Footer />*/}
+
+      <Footer />
 
       <GlobalStyles />
     </BrowserRouter>
